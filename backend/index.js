9143c1f16ef5a1ff664ef7d79c9f8e77707b0c1b
shritiz k <shritizk@gmail.com>
2024-05-24 11:01:43 +0530

    ready to test

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const { studentRouter } = require('./routers/studenRouter');
+const { bookRouter } = require('./routers/booksRouter');
 const app =express();
+app.use(express.json())
+
+app.router('student/', studentRouter )
+app.router('book/',bookRouter)
+
+app.listen(3000 , ()=>{console.log("server online")})
+
 
 
 
